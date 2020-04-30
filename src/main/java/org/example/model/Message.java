c0858fd97430a5f3641ba501ea9de2f0769edf95
Vlad1235 <fn1235@bk.ru>
2020-05-01 00:56:24 +0900

    base

---
+++ src/main/java/org/example/model/Message.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+import java.util.Date;
+
+public class Message {
+
+  private long id;
+  private String message;
+  private Date created;
+  private String author;
+}
